cfc1d591fd408abb96c6a2cf8004ee2d45beec34
willson <willhausmarc@gmail.com>
2024-10-11 22:12:19 +0200

    adjusted deconding for new line encoded strings

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ output_file_path = "o200k_base.txt"
 
 
 # Decodes the input key and returns the decoded string
+# The \n and \r characters are replaced with spaces or empty strings
 def decode(k):
-    return str(enc.decode_bytes([k]).decode("utf-8", errors="replace"))
+    return (
+        str(enc.decode_bytes([k]).decode("utf-8", errors="replace"))
+        .replace("\n", " ")
+        .replace("\r", "")
+    )
 
 
 # Write the decoded strings to the output file
